4f5bd4711fd7be3c3ff2722f1ac0a75d252f119e
Myshonkov Maxim <maxim.myshonkov@gmail.com>
2021-07-19 12:12:28 +0300

    Add manifest summary

---
+++ internal/pkg/manifest/manifest.go
@@ -0,0 +1,57 @@
+package manifest
+
+import (
+	"encoding/json"
+	"errors"
+	"github.com/docker/distribution/manifest/schema1"
+	"github.com/docker/distribution/manifest/schema2"
+	"io"
+	"io/ioutil"
+	"net/http"
+)
+
+var ErrApiStatusCode = errors.New("docker registry API returned error status")
+
+func GetManifestData(manifestUrl string, manifestTypeHeader string) (manifestData []byte, apiResp *http.Response, err error) {
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", manifestUrl, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+	req.Header.Set("Accept", manifestTypeHeader)
+
+	apiResp, err = client.Do(req)
+	if err != nil {
+		return nil, nil, err
+	}
+	defer func(Body io.ReadCloser) {
+		_ = Body.Close()
+	}(apiResp.Body)
+
+	if apiResp.StatusCode != 200 {
+		return nil, apiResp, ErrApiStatusCode
+	}
+
+	jsonManifestData, err := ioutil.ReadAll(apiResp.Body)
+	return jsonManifestData, apiResp, err
+}
+
+func GetV1Manifest(manifestUrl string) (manifest *schema1.Manifest, apiResp *http.Response, err error) {
+	jsonManifestData, apiResp, err := GetManifestData(manifestUrl, schema1.MediaTypeManifest)
+	if err != nil {
+		return nil, nil, err
+	}
+	manifest = &schema1.Manifest{}
+	err = json.Unmarshal(jsonManifestData, manifest)
+	return manifest, apiResp, err
+}
+
+func GetV2Manifest(manifestUrl string) (manifest *schema2.Manifest, apiResp *http.Response, err error) {
+	jsonManifestData, apiResp, err := GetManifestData(manifestUrl, schema2.MediaTypeManifest)
+	if err != nil {
+		return nil, nil, err
+	}
+	manifest = &schema2.Manifest{}
+	err = json.Unmarshal(jsonManifestData, manifest)
+	return manifest, apiResp, err
+}
--- internal/pkg/registry_api/registry_api.go
+++ internal/pkg/registry_api/registry_api.go
@@ -2,11 +2,15 @@ package registry_api
 
 import (
 	"encoding/json"
+	"github.com/tidwall/gjson"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
 	"path"
+	"registry-cleaner-agent/internal/pkg/manifest"
 	"registry-cleaner-agent/internal/pkg/status"
+	"strings"
+	"time"
 )
 
 type RegistryApiHandler struct {
@@ -47,7 +51,6 @@ func (rah *RegistryApiHandler) ProxyHandler(w http.ResponseWriter, r *http.Reque
 func (rah *RegistryApiHandler) StatusHandler(w http.ResponseWriter, _ *http.Request) {
 	healthUrl, err := url.Parse(rah.ApiUrl.String())
 	if err != nil {
-		// TODO: log errors
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
@@ -56,6 +59,59 @@ func (rah *RegistryApiHandler) StatusHandler(w http.ResponseWriter, _ *http.Requ
 	resp, err := http.Get(healthUrl.String())
 	rah.Status.IsAlive = err == nil && resp.StatusCode == 200
 	res, err := json.Marshal(&rah.Status)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	_, _ = w.Write(res)
+}
+
+func (rah *RegistryApiHandler) MafifestSummaryHandler(w http.ResponseWriter, r *http.Request) {
+	manifestUrl := *rah.ApiUrl
+	manifestPath := strings.TrimSuffix(r.URL.Path, "/summary")
+	manifestUrl.Path = path.Join(manifestUrl.Path, manifestPath)
+
+	v1Manifest, v1ApiResp, err := manifest.GetV1Manifest(manifestUrl.String())
+	if err == manifest.ErrApiStatusCode && v1ApiResp != nil {
+		http.Error(w, v1ApiResp.Status, v1ApiResp.StatusCode)
+		return
+	}
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	v2Manifest, v2ApiResp, err := manifest.GetV2Manifest(manifestUrl.String())
+	if err == manifest.ErrApiStatusCode && v2ApiResp != nil {
+		http.Error(w, v2ApiResp.Status, v2ApiResp.StatusCode)
+		return
+	}
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	imageSize := v2Manifest.Config.Size
+	for _, layer := range v2Manifest.Layers {
+		imageSize += layer.Size
+	}
+	creationTime := time.RFC3339
+	if len(v1Manifest.History) != 0 {
+		value := gjson.Get(v1Manifest.History[0].V1Compatibility, "created")
+		creationTime = value.Str
+	}
+	digest := v2ApiResp.Header.Get("Docker-Content-Digest")
+	manifestSummary := manifest.Summary{
+		Name:          v1Manifest.Name,
+		Tag:           v1Manifest.Tag,
+		Architecture:  v1Manifest.Architecture,
+		Created:       creationTime,
+		Size:          imageSize,
+		ContentDigest: digest,
+	}
+
+	res, err := json.Marshal(&manifestSummary)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
